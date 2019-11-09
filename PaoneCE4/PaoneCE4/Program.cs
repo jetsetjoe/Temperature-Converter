4abbf87f1544af7f2dcc264b6dc4eaada608e60b
Joseph Paone <josephjpaone@gmail.com>
2019-11-08 19:38:13 -0600

    Add files via upload
    Uploaded files. 
    This project was an undergraduate project stored on googled drive.

---
+++ PaoneCE4/PaoneCE4/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaoneCE4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //declare variables
+
+            double
+                tempF=0,
+                tempCelsius=0;
+
+            //ask for user input
+
+            Console.WriteLine("Please enter the temperature in fahrenheit: ");
+          
+
+            tempConvert(ref tempCelsius);
+            
+
+            Console.WriteLine("Converted value is: {0} degrees Celsius", +Math.Round(tempCelsius, 0));
+
+
+            Console.ReadKey();
+
+
+
+
+        }//end of main
+
+
+
+        public static double tempConvert(ref double tempCelsius)
+        {
+            double tempF;
+           //do conversions
+            tempF = Convert.ToDouble(Console.ReadLine());
+            tempCelsius = (tempF - 32) * 5.0/9.0;
+            return tempCelsius;
+
+        }
+
+
+
+
+
+    }//end of class
+
+}
